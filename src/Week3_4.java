d22d8e77b165180a9f955ea736eee300e4a943f8
Arjun <arjunanand206@gmail.com>
2026-04-08 08:47:19 +0530

    Q4

--- src/Week3_4.java
+++ src/Week3_4.java
@@ -2,69 +2,87 @@ import java.util.*;
 
 public class Week3_4 {
 
-    static void mergeSort(int[] arr, int l, int r) {
+    static class Asset {
+        String name;
+        double returnRate;
+        double volatility;
+
+        Asset(String name, double r, double v) {
+            this.name = name;
+            this.returnRate = r;
+            this.volatility = v;
+        }
+    }
+
+    static void mergeSort(Asset[] arr, int l, int r) {
         if (l < r) {
             int mid = (l + r) / 2;
-
             mergeSort(arr, l, mid);
             mergeSort(arr, mid + 1, r);
             merge(arr, l, mid, r);
         }
     }
 
-    static void merge(int[] arr, int l, int mid, int r) {
-        int[] temp = new int[r - l + 1];
-        int i = l, j = mid + 1, k = 0;
+    static void merge(Asset[] arr, int l, int m, int r) {
+        Asset[] temp = new Asset[r - l + 1];
+        int i = l, j = m + 1, k = 0;
 
-        while (i <= mid && j <= r) {
-            if (arr[i] <= arr[j]) temp[k++] = arr[i++];
-            else temp[k++] = arr[j++];
+        while (i <= m && j <= r) {
+            if (arr[i].returnRate <= arr[j].returnRate)
+                temp[k++] = arr[i++];
+            else
+                temp[k++] = arr[j++];
         }
 
-        while (i <= mid) temp[k++] = arr[i++];
+        while (i <= m) temp[k++] = arr[i++];
         while (j <= r) temp[k++] = arr[j++];
 
-        for (int x = 0; x < temp.length; x++) {
+        for (int x = 0; x < temp.length; x++)
             arr[l + x] = temp[x];
-        }
     }
 
-    static void quickSort(int[] arr, int low, int high) {
+    static void quickSort(Asset[] arr, int low, int high) {
         if (low < high) {
             int pi = partition(arr, low, high);
-
             quickSort(arr, low, pi - 1);
             quickSort(arr, pi + 1, high);
         }
     }
 
-    static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
+    static int partition(Asset[] arr, int low, int high) {
+        Asset pivot = arr[high];
         int i = low - 1;
 
         for (int j = low; j < high; j++) {
-            if (arr[j] > pivot) { // DESC
+            if (arr[j].returnRate > pivot.returnRate ||
+                    (arr[j].returnRate == pivot.returnRate &&
+                            arr[j].volatility < pivot.volatility)) {
+
                 i++;
-                int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+                Asset temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
             }
         }
 
-        int temp = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = temp;
+        Asset temp = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = temp;
         return i + 1;
     }
 
     public static void main(String[] args) {
 
-        int[] arr = {500, 100, 300};
+        Asset[] arr = {
+                new Asset("AAPL", 12, 5),
+                new Asset("TSLA", 8, 7),
+                new Asset("GOOG", 15, 4)
+        };
 
         mergeSort(arr, 0, arr.length - 1);
-        System.out.println("Merge Sort (ASC): " + Arrays.toString(arr));
+        System.out.println("Merge Sort ASC:");
+        for (Asset a : arr)
+            System.out.println(a.name + " " + a.returnRate);
 
         quickSort(arr, 0, arr.length - 1);
-        System.out.println("Quick Sort (DESC): " + Arrays.toString(arr));
-
-        int total = 0;
-        for (int x : arr) total += x;
-        System.out.println("Total Volume: " + total);
+        System.out.println("\nQuick Sort DESC:");
+        for (Asset a : arr)
+            System.out.println(a.name + " " + a.returnRate);
     }
 }
